9a2be2e5793e9fc88048d7908033c392961eef6b
Hyun <hyun@balancehero.com>
2020-07-23 00:41:09 +0900

    move unused code

--- sample-need-refactoring/sample-testing-codelab/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/TodoFragmentTest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.android.architecture.blueprints.todoapp
-
-import kim.jeonghyeon.androidlibrary.architecture.mvvm.BaseFragment
-import kim.jeonghyeon.androidtesting.BaseFragmentTest
-
-abstract class TodoFragmentTest<T : BaseFragment> : BaseFragmentTest<T>() {
-    override val theme: Int = R.style.AppTheme
-}
