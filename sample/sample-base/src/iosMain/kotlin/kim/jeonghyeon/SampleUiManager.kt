dd9f5dc1975144f9a0ade16247d0c8ac520c4a86
Hyun <hyun@balancehero.com>
2020-10-26 00:11:29 +0900

    KSA-44  Hyun  [Android] Compose Navigation

---
+++ sample/sample-base/src/iosMain/kotlin/kim/jeonghyeon/SampleUiManager.kt
@@ -0,0 +1,8 @@
+package kim.jeonghyeon
+
+import kim.jeonghyeon.ui.UiManager
+
+
+//todo if this is in library module. it's not recognized.
+// if it's recognized, remove this and use UiManager.
+class SampleUiManager : UiManager()
